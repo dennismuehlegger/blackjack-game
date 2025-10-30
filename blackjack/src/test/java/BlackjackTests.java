b28b6e176eaa7052e2b25dcd7761fa722594ea70
MHLD <Dennis.Muehlegger@partner.bmw.de>
2025-10-30 09:13:53 +0100

    initial tests added and output adjustments

--- blackjack/src/test/java/BlackjackTests.java
+++ blackjack/src/test/java/BlackjackTests.java
@@ -1,19 +1,20 @@
-/*
-package java;
 
+package test.java;
+
+import main.java.Game.BlackjackGame;
+import main.java.Game.Card;
+import main.java.Game.Player;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class BlackjackTests {
 
-    @Before
+    /*@Before
     public void init() {
 
     }
@@ -21,9 +22,29 @@ public class BlackjackTests {
     @After
     public void cleanup() {
 
+    }*/
+
+    @Test
+    public void testPlayerBust() {
+        Player player = new Player("dennis");
+        player.addCard(new Card(10, "♠", "K"));
+        player.addCard(new Card(10, "♥", "Q"));
+        player.addCard(new Card(5, "♦", "5"));
+
+        assertTrue(player.isBusted());
+        assertEquals(25, player.getHandValue());
     }
 
     @Test
-    public void test() {
+    public void testPlayerNoBust() {
+        Player player = new Player("dennis");
+        player.addCard(new Card(10, "♠", "K"));
+        player.addCard(new Card(10, "♥", "Q"));
+        // ace calculation works as well
+        player.addCard(new Card(1, "♦", "A"));
+
+        assertFalse(player.isBusted());
+        assertEquals(21, player.getHandValue());
     }
-}*/
+
+}
